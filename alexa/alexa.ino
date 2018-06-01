f5df66b9135bf670ebe896ee746a22c025c8f476
Yoimer <ydrf19@gmail.com>
2018-06-01 14:42:50 -0400

    trying publishing humidiy and temperature to ubidot

--- alexa/alexa.ino
+++ alexa/alexa.ino
@@ -1,37 +1,9 @@
-// #include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
-// #define DHTTYPE DHT11   // DHT 11
-
-// #define dht_dpin 0
-// DHT dht(dht_dpin, DHTTYPE); 
-// void setup(void)
-// { 
-//   dht.begin();
-//   Serial.begin(9600);
-//   Serial.println("Humidity and temperature\n\n");
-//   delay(700);
-
-// }
-// void loop() {
-//     readTemperatureAndHumidity();
-// }
-
-// ////readTemperatureAndHumidity
-// float readTemperatureAndHumidity() {
-//     float h = dht.readHumidity();
-//     float t = dht.readTemperature();         
-//     Serial.print("Current humidity = ");
-//     Serial.print(h);
-//     Serial.print("%  ");
-//     Serial.print("temperature = ");
-//     Serial.print(t); 
-//     Serial.println("C  ");
-//     delay(800);
-// }
-
 /****************************************
  * Include Libraries
  ****************************************/
 #include "UbidotsESPMQTT.h"
+#include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
+#define dht_dpin 0
 
 /****************************************
  * Define Constants
@@ -39,9 +11,13 @@
 #define TOKEN "A1E-kVgDgXsJGczuXn5745CwVSKFS7UqBnXBzqHzARChQzBlipec3vZbo7kx" // Your Ubidots TOKEN
 #define WIFINAME "SKY7D85E" //Your SSID
 #define WIFIPASS "8121985" // Your Wifi Pass
+#define DHTTYPE DHT11   // DHT 11
+// global values for humidity and temperature
+float h = -1.0;
+float t = -1.0;
 
 Ubidots client(TOKEN);
-
+DHT dht(dht_dpin, DHTTYPE); 
 /****************************************
  * Auxiliar Functions
  ****************************************/
@@ -56,6 +32,19 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println();
 }
 
+////readTemperatureAndHumidity
+float readTemperatureAndHumidity() {
+    h = dht.readHumidity();
+    t = dht.readTemperature();
+    Serial.print("Current humidity = ");
+    Serial.print(h);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(t); 
+    Serial.println("C  ");
+    delay(800);
+}
+
 /****************************************
  * Main Functions
  ****************************************/
@@ -65,20 +54,24 @@ void setup() {
   Serial.begin(115200);
   client.setDebug(true); // Pass a true or false bool value to activate debug messages
   client.wifiConnection(WIFINAME, WIFIPASS);
+  dht.begin();
+  Serial.println("Humidity and temperature\n\n");
+  delay(700);
   client.begin(callback);
   }
 
 void loop() {
+  // read temperature and humidity
+  readTemperatureAndHumidity();
   // put your main code here, to run repeatedly:
   if(!client.connected()){
       client.reconnect();
       }
   
   // Publish values to 2 different data sources
-  
-  client.add("stuff", 10.2); //Insert your variable Labels and the value to be sent
+  client.add("stuff", h); //Insert your variable Labels and the value to be sent
   client.ubidotsPublish("source1");
-  client.add("stuff", 10.2);
+  client.add("stuff", t);
   client.add("more-stuff", 120.2);
   client.ubidotsPublish("source2");
   client.loop();
